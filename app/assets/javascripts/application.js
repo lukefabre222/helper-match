a298cd0c0cb6ee63b4cce3f1cd352da33ebba761
TATSUYA <tatsuya63177136@gmail.com>
2020-09-04 23:59:54 +0900

    edit top-page jquery css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -67,4 +67,35 @@ $(function () {
         });
     }
 });
+
+$(window).scroll(function (){
+  $('.fadein').each(function(){
+      var elemPos = $(this).offset().top,
+          scroll = $(window).scrollTop(),
+          windowHeight = $(window).height();
+        if (scroll > elemPos - windowHeight + 100){
+            $(this).addClass('scrollin');
+          }
+      });
+});
+$(window).scroll(function (){
+    $('.fadeinleft').each(function(){
+      var elemPos = $(this).offset().top,
+          scroll = $(window).scrollTop(),
+          windowHeight = $(window).height();
+        if (scroll > elemPos - windowHeight + 100){
+            $(this).addClass('scrollin');
+          }
+      });
+  });
+$(window).scroll(function (){
+  $('.fadeinright').each(function(){
+      var elemPos = $(this).offset().top,
+          scroll = $(window).scrollTop(),
+          windowHeight = $(window).height();
+        if (scroll > elemPos - windowHeight + 100){
+            $(this).addClass('scrollin');
+          }
+      });
+  });
 });
