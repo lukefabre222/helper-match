85fb85725623a8cee0efc7789a3496fa45c20a17
TATSUYA <tatsuya63177136@gmail.com>
2020-09-15 13:16:50 +0900

    edit average_value of rader-chart

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -7,5 +7,9 @@ module ReportsHelper
   def total_work_days(id)
     Report.where(staff_id: id).where(worked_on: (Date.today.beginning_of_month - 180.day)..(Date.today.end_of_month)).count
   end
+
+  def average_value(column)
+    Report.where(worked_on: (Date.today.beginning_of_month - 180.day)..(Date.today.end_of_month)).average(column).round(1)
+  end
   
 end
