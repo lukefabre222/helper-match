57431b995a16abfa1bfc0cae55b8dcd2f98147ad
TATSUYA <tatsuya63177136@gmail.com>
2020-10-21 23:16:11 +0900

    edit staff_show index

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -11,4 +11,16 @@ module EventsHelper
   def price_sum(staff_name)
     Event.where(staff_name: staff_name).where(start_date: (Date.today.beginning_of_month)...(Date.today.end_of_month)).where(status: '確定').sum(:price)
   end
+
+  def request_sum_shop(shop_name)
+    Event.where(shop_name: shop_name).where(start_date: (Date.today.beginning_of_month)...(Date.today.end_of_month)).where(status: "申請中").count
+  end
+
+  def work_sum_shop(shop_name)
+    Event.where(shop_name: shop_name).where(start_date: (Date.today.beginning_of_month)...(Date.today.end_of_month)).where(status: '確定').count
+  end
+
+  def price_sum_shop(shop_name)
+    Event.where(shop_name: shop_name).where(start_date: (Date.today.beginning_of_month)...(Date.today.end_of_month)).where(status: '確定').sum(:price)
+  end
 end
